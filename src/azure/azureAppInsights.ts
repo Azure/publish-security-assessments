92b19fb8b2b5b8ca5d3544cbe7e041d139770ee9
Ajinkya <11447401+ajinkya599@users.noreply.github.com>
2021-03-05 14:50:50 +0530

    Fixes #4 and #5 (#7) - Add requestId in logs, Duplicate CVEs in result list

--- src/azure/azureAppInsights.ts
+++ src/azure/azureAppInsights.ts
@@ -3,13 +3,16 @@
 
 import * as core from '@actions/core';
 import * as Inputs from '../utils/inputs';
+import * as ApplicationInsights from 'applicationinsights';
+import { ScanProperties } from './scanResultContracts';
 let appInsights = require('applicationinsights');
 
-export function postEvent(eventData: any): void {
+export function postEvent(eventData: ApplicationInsights.Contracts.EventTelemetry): void {
   appInsights.setup(Inputs.authKey);
   const appInsightsClient = appInsights.defaultClient;
+  const requestId = (eventData.properties as ScanProperties).context.requestId;
 
-  console.log('Posting scan results to Azure AppInsights...');
+  console.log(`Posting scan results to Azure AppInsights. Request Id: ${requestId}`);
   core.debug(`Event data: ${JSON.stringify(eventData)}`);
 
   appInsightsClient.trackEvent(eventData);
