fe6e5a2ba5e83fd2532175c83f0f0e235f8afcb6
Sundar Guntnur <sundargs2000@github.com>
2021-04-01 16:30:56 +0530

    Added L0 tests.

--- src/run.ts
+++ src/run.ts
@@ -5,7 +5,7 @@ import * as core from '@actions/core';
 import * as Inputs from './utils/inputs';
 import { postScanResults } from './utils/utilities';
 
-async function run() {
+export async function run() {
   try {
     Inputs.readInputs();
     await postScanResults();
